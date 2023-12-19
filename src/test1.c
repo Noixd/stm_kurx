1b68db8875e449e95224a2f168f51a460a8e792a
Noix <noiodox@mail.ru>
2023-12-19 11:35:25 +0300

    interrupt set

--- src/test1.c
+++ src/test1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+static bool on_relay;
 
 void InitPin() {
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
@@ -22,36 +23,38 @@ void InitPin() {
 void InitInterrupt() {
     AFIO->EXTICR[0] &= ~(AFIO_EXTICR1_EXTI0);
 	EXTI -> IMR |= EXTI_IMR_MR0; // настройка регистра генерации прерывания
-	EXTI -> RTSR |= EXTI_RTSR_TR0;	// срабатывание при падении напряжения
-	NVIC_EnableIRQ( ) // Настройка перехода на функцию прерывания 
+	EXTI -> FTSR |= EXTI_FTSR_TR0;	// срабатывание при падении напряжения
+	NVIC_EnableIRQ(EXTI0_IRQn); // Настройка перехода на функцию прерывания 
 }
-
 
 int main() {
 
-    bool Off, On;
-
     InitPin();
+    InitInterrupt();
 
-while (1) {
-    
-
-
-    if((GPIOA->IDR &= GPIO_IDR_IDR1) == 0) //условие нажатия на кнопку
-		{
-			GPIOC->ODR |= GPIO_ODR_ODR13; 
-		}
-        else 
-        {
-            GPIOC->ODR &= ~GPIO_ODR_ODR13; 
-        }
-}
-return 0;
+    while (1) {
+        if(on_relay == true) {
+                GPIOC->ODR |= GPIO_ODR_ODR13; 
+            }
+            else {
+                GPIOC->ODR &= ~GPIO_ODR_ODR13; 
+            }
+    }
+    return 0;
 }
 
-	void EXTI_1_IRQHandler(void) {
-		if ((EXTI -> PR & EXT_PR_PR0) == (0X01 << EXTI_PR_PR0_Pos)) {
-			EXTI -> PR |= EXT_PR_PR0; // сброс события прерывания
-			GPIOB -> ODR ^= GPIO_ODR_8; // иневерсия PB8
+void EXTIO_IRQHandler(void) {
+	if ((EXTI -> PR & EXTI_PR_PR0) == (0X01 << EXTI_PR_PR0_Pos)) {
+
+		EXTI -> PR |= EXTI_PR_PR0; // сброс события прерывания
+
+        switch (on_relay) {
+            case true:
+                on_relay = false;
+                break;
+            case false:
+                on_relay = true;
+                break;
+            }
 		}
-	} // функция прерывания 
+	}
