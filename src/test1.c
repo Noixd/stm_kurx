99cd7ca39b95c9c32e676d1ef5819d4f28ff3902
Noix <noiodox@mail.ru>
2023-12-20 20:03:05 +0300

    upd

--- src/test1.c
+++ src/test1.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
- bool on_relay = false;
+static bool on_relay = false;
+static uint8_t rx = 0;
+static uint8_t tx = 0;
+
 
 void InitPin() {
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
@@ -32,11 +35,18 @@ void InitInterrupt() {
 	NVIC_EnableIRQ(EXTI1_IRQn); // Настройка перехода на функцию прерывания 
 }
 
+void InitUsart1() {
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN; //настройка тактирования для USART USART1->CR1 |= USART_CR_OVER8; 
+    USART1->BRR = 9600; // собственно скорость
+    USART1->CR1 |= USART_CR1_RXNEIE | USART_CR1_RE | USART_CR1_UE; // включение прерывания, включение приемника, включение UART
+    NVIC_SetPriority(USART1_IRQn, 0); // выбор приоритета NVIC_EnableIRQ(USART1_IRQn); // разрешение прерывания
+}
+
 int main() {
 
     InitPin();
     InitInterrupt();
-//(GPIOA->IDR &= GPIO_IDR_IDR1)==0
+
     while (1) {
         GPIOC -> ODR |= GPIO_ODR_ODR15;
         for (uint32_t i = 0; i < 1000000; i++) asm("nop");
@@ -45,18 +55,20 @@ int main() {
     }
     return 0;
 }
-// if ((EXTI -> PR & EXTI_PR_PR1) == (0X01 << EXTI_PR_PR0_Pos))
-void EXTI1_IRQHandler(void) {
-	//if ((EXTI -> PR & EXTI_PR_PR1) == ~(0X01 << EXTI_PR_PR0_Pos)) 
-    {
 
-		EXTI -> PR = EXTI_PR_PR1; // сброс события прерывания
+// if ((EXTI -> PR & EXTI_PR_PR1) == (0X01 << EXTI_PR_PR0_Pos))
+//(GPIOA->IDR &= GPIO_IDR_IDR1)==0
+void EXTI1_IRQHandler(void) {
+    
+	EXTI -> PR = EXTI_PR_PR1; // сброс события прерывания
+
     if(on_relay) {
                 GPIOC->ODR &= ~GPIO_ODR_ODR13; 
             }
             else {
                 GPIOC->ODR |= GPIO_ODR_ODR13; 
             }
+
         switch (on_relay) {
             case true:
                 on_relay = false;
@@ -65,5 +77,10 @@ void EXTI1_IRQHandler(void) {
                 on_relay = true;
                 break;
             }
-		}
 	}
+
+void USART1_IRQnHandler(void) {
+    if ((USART1 -> ISR & USART_) == 0x01 << 5) {
+        
+    }
+}
